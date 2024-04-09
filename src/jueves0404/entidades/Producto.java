8f722ff8a7d03c542eb1f4f0fb71b4208b159115
josiastolaba <163350052+josiastolaba@users.noreply.github.com>
2024-04-09 16:23:43 -0300

    Initial commit

---
+++ src/jueves0404/entidades/Producto.java
@@ -0,0 +1,92 @@
+
+package jueves0404.entidades;
+
+
+public class Producto {
+    private int codigo;
+    private String descripcion;
+    private double precio;
+    private int stock;
+    private String categoria;
+
+    public Producto() {
+    }
+
+    public Producto(int codigo, String descripcion, double precio, int stock, String categoria) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.stock = stock;
+        this.categoria = categoria;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + this.codigo;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Producto other = (Producto) obj;
+        if (this.codigo != other.codigo) {
+            return false;
+        }
+        return true;
+    }
+    
+    public String toString(){
+    
+        return codigo+" "+descripcion;
+    }
+}
